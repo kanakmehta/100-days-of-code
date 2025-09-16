779ddeba278d60f9655f5a71c82be308df17b88e
Kanak Mehta <kanakmehta1509@gmail.com>
2025-09-16 15:36:17 +0530

    Update 02_ques2.py

---
+++ 02_ques2.py
@@ -0,0 +1,7 @@
+# Write a program to calculate the area and circumference of a circle given its radius.
+print("Enter the radius of the circle:")
+radius = float(input())
+area = 3.14 * radius * radius
+circumference = 2 * 3.14 * radius
+print(f"area is {area}")
+print(f"circumference is {circumference}")
